f88d324bee4adc39b54187daf99a08a7f11b85c9
Hossein Shojaei <52251708+HosseinShojaei@users.noreply.github.com>
2023-05-04 23:47:48 +0330

    Add files via upload

---
+++ stirredJacketedTank/Simulink/Initial_data.m
@@ -0,0 +1,31 @@
+clc
+clear
+
+%% Steady-State
+F_s = 1;
+F_js = 1.5;
+T_is = 50;
+T_jis = 200;
+
+T_s = 125;
+T_js = 150;
+
+%% Optional Changes
+F  = 1.5;
+F_j = 2.7;
+T_i = 57;
+T_ji = 206;
+
+%% Small Changes
+SF  = 1.2;
+SF_j = 1.7;
+ST_i = 55;
+ST_ji = 210;
+
+%% Large Changes
+LF  = 1.6;
+LF_j = 1.9;
+LT_i = 70;
+LT_ji = 240;
+
+
